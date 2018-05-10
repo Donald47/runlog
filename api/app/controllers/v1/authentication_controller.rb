9ce4d0b434a906803c4178d583d4b31d0bf5cd5b
Donald <mackintoshd@hotmail.co.uk>
2018-05-11 00:13:25 +0100

    finalise api form for auth

--- api/app/controllers/v1/authentication_controller.rb
+++ api/app/controllers/v1/authentication_controller.rb
@@ -3,18 +3,19 @@ module V1
     skip_before_action :require_auth
 
     def create
+      error_json = { message: 'Invalid Details' }
       athelete = Athelete.find_by(email: params[:email])
-      render json: {}, status: :unauthorized if athelete.nil?
-      render json: {}, status: :unauthorized if athelete.authenticate(params[:password]) == false
+      return render json: error_json, status: :unauthorized if athelete.nil?
+      return render json: error_json, status: :unauthorized if athelete.authenticate(params[:password]) == false
       token = Tokeniser.encode(athelete.id)
       render json: { auth_token: token }, status: :ok
     rescue StandardError => e
       Rails.logger.error(e)
-      render json: {}, status: :internal_server_error
+      render json: error_json, status: :internal_server_error
     end
 
     def destroy
-
+      
     end
   end
 end
